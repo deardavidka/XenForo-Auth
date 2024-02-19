fbf337a563e2e6ed2b939c0eab2abf1eb14ec33f
RebornStudio <147333820+deardavidka@users.noreply.github.com>
2024-02-19 12:50:07 +0500

    Update main.cpp

--- xenforo-auth/xenforo-auth/main.cpp
+++ xenforo-auth/xenforo-auth/main.cpp
@@ -1,10 +1,5 @@
 /*
-RU: Здравствуйте! Это лёгкий пример как можно реализовать авторизацию на вашем xenforo 
-    форуме используя нативный C++. Пользуясь случаем сообщу вам о том, что если вы хотите сделать 
-    свой лоадер, чит, программу на C++ и у вас не получается с дизайном, то смело обращайтесь в 
-    RebornStudio (ссылка ниже), по промокоду GITHUB вы получите дополнительную скидку в 5%
-
-EN: Hello! This is an easy example of how you can implement authorization on your xenforo forum using 
+Hello! This is an easy example of how you can implement authorization on your xenforo forum using 
     native C++. I will take this opportunity to inform you that if you want to make your loader, 
     cheat, or C++ program and you are having trouble with the design, then feel free to contact 
     RebornStudio (link below), using the GITHUB promo code you will receive an additional 5% discount%
@@ -20,6 +15,6 @@ int main() {
     trik.auth();
     trik.request();
     trik.parse();
-    /*trik.parse_selfdata(); - uncomment it if you want to take specific inf. | разкоментить если вам нужна конкретная информация */
+    /*trik.parse_selfdata(); - uncomment it if you want to take specific inf. | Г°Г Г§ГЄГ®Г¬ГҐГ­ГІГЁГІГј ГҐГ±Г«ГЁ ГўГ Г¬ Г­ГіГ¦Г­Г  ГЄГ®Г­ГЄГ°ГҐГІГ­Г Гї ГЁГ­ГґГ®Г°Г¬Г Г¶ГЁГї */
     return 0; 
 }
